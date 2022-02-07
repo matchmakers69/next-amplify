f55424961bd212935c016d0df717e02c71a556cc
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-07 23:55:38 +0000

    Updated prettier config

--- src/pages/__tests__/index.spec.js
+++ src/pages/__tests__/index.spec.js
@@ -3,13 +3,13 @@ import renderer from "react-test-renderer";
 import Home from "..";
 
 describe("<Home />", () => {
-	it("should create a snapshot for Home Page", () => {
-		expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
-	});
-	it("renders Home page without crashing", () => {
-		const { getByTestId } = render(<Home />);
+    it("should create a snapshot for Home Page", () => {
+        expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
+    });
+    it("renders Home page without crashing", () => {
+        const { getByTestId } = render(<Home />);
 
-		const pageWrapper = getByTestId("page-wrapper");
-		expect(pageWrapper).toBeInTheDocument();
-	});
+        const pageWrapper = getByTestId("page-wrapper");
+        expect(pageWrapper).toBeInTheDocument();
+    });
 });
