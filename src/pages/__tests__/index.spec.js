796ba5bebde4038f9c9e2ed9468099db8ad7f5fc
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-09 22:37:48 +0000

    Updated test and settings

--- src/pages/__tests__/index.spec.js
+++ src/pages/__tests__/index.spec.js
@@ -1,13 +1,27 @@
 import { render } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { theme } from "src/themes/theme";
 import Home from "..";
 
 describe("<Home />", () => {
   it("should create a snapshot for Home Page", () => {
-    expect(renderer.create(<Home />).toJSON()).toMatchSnapshot();
+    expect(
+      renderer
+        .create(
+          <StyledThemeProvider theme={theme}>
+            <Home />
+          </StyledThemeProvider>
+        )
+        .toJSON()
+    ).toMatchSnapshot();
   });
   it("renders Home page without crashing", () => {
-    const { getByTestId } = render(<Home />);
+    const { getByTestId } = render(
+      <StyledThemeProvider theme={theme}>
+        <Home />
+      </StyledThemeProvider>
+    );
 
     const pageWrapper = getByTestId("page-wrapper");
     expect(pageWrapper).toBeInTheDocument();
