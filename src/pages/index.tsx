4e47a8be8c93c1d59edda6c72056419cd9f90696
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-09 21:23:34 +0000

    Updated _document and snapdhot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import Meta from "src/components/Meta";
+
 const Home = () => {
-  return <div data-testid="page-wrapper">Home Page</div>;
+  return (
+    <>
+      <Meta
+        schemaType="Welcome home page"
+        title="The Fate of Empires"
+        description="The only thing we learn from history, it has been said, 'is that men never learn from history'..."
+      />
+
+      <div data-testid="page-wrapper">
+        <h1>Welcome to Website</h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
