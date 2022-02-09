a349802351a2130447f2055262c773b7ae6eb7fc
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-09 21:20:11 +0000

    Updated _app and _document added mui and styled-components configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import createEmotionCache from "src/createEmotionCache";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import Layout from "src/components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,64 @@
+import { ServerStyleSheets } from "@mui/styles";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "src/createEmotionCache";
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+    // However, be aware that it can have global side effects.
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props) => sheet.collectStyles(materialSheets.collect(<App emotionCache={cache} {...props} />))
+        });
+
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      const emotionStyles = extractCriticalToChunks(initialProps.html);
+
+      const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style data-emotion={`${style.key} ${style.ids.join(" ")}`} key={style.key} dangerouslySetInnerHTML={{ __html: style.css }} />
+      ));
+
+      return {
+        ...initialProps,
+        emotionStyleTags,
+        styles: (
+          <>
+            {initialProps.styles}
+            {materialSheets.getStyleElement()}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;900&family=Roboto+Slab:wght@400;600&display=swap" rel="stylesheet" />
+          {/* Inject MUI styles first to match with the prepend: true configuration. */}
+          {(this.props as any).emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
