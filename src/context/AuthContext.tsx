750c82237b343977e1ce905f16046a93013685f7
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-10 14:12:23 +0000

    Updated AuthContext - added UserContextType useUser

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,42 @@
-import { createContext, ReactElement, useState } from "react";
+import { createContext, Dispatch, ReactElement, SetStateAction, useContext, useEffect, useState } from "react";
 import { CognitoUser } from "@aws-amplify/auth";
+import { Hub, Auth } from "aws-amplify";
 
-const UserContext = createContext();
+interface UserContextType {
+  user: CognitoUser | null;
+  setUser: Dispatch<SetStateAction<CognitoUser | null>>;
+}
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
 
 interface IUserContextProps {
   children: React.ReactElement;
 }
 
-const AuthContext = ({ children }: IUserContextProps): ReactElement => {
+export default function AuthContext({ children }: IUserContextProps): ReactElement {
   const [user, setUser] = useState<CognitoUser | null>(null);
+  const checkUser = async () => {
+    try {
+      const amplifyUser = await Auth.currentAuthenticatedUser();
+      if (amplifyUser) {
+        setUser(amplifyUser);
+      }
+    } catch (error) {
+      console.log(error);
+      // No current Signed user
+      setUser(null);
+    }
+  };
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  useEffect(() => {
+    Hub.listen("auth", () => {
+      // perform some action to update state
+      checkUser();
+    });
+  }, []);
   return (
     <UserContext.Provider
       value={{
@@ -19,6 +47,6 @@ const AuthContext = ({ children }: IUserContextProps): ReactElement => {
       {children}
     </UserContext.Provider>
   );
-};
+}
 
-export default AuthContext;
+export const useUser = (): UserContextType => useContext(UserContext);
